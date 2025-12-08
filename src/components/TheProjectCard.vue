9b32e5ce2b301d05b289421990110bd8425a03b5
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-12-08 22:09:25 +0100

    add about page

--- src/components/TheProjectCard.vue
+++ src/components/TheProjectCard.vue
@@ -1,17 +1,19 @@
 <script setup>
 import data from "../db/projects.json";
-const preview = data.portfolio;
+// keep only the last 3 projects from the array
+const preview = data.portfolio.slice(-3);
+import resolveIllustration from "../utils/illustrationsResolver.js";
 </script>
 
 <template>
     <section class="flex flex-wrap justify-center gap-4 mt-12 mb-12">
-         <div v-for="(project, index) in preview.reverse().slice(0,3)" :key="index"
-            class="lg:basis-1/4 md:basis-1/3 sm:basis-full shrink-0 bg-amber-50 border hover:drop-shadow-lg">
-            <router-link :to="'/projects/' + project.id"
-                class="inline-flex items-center text-sm font-medium">
-                <img class=" object-cover" src="../assets/stoic-700-800.png" alt="" />
+    <div v-for="(project, index) in preview" :key="project.id"
+            class="text-center lg:basis-1/4 md:basis-1/3 sm:basis-full shrink-0 bg-amber-50 border hover:drop-shadow-lg">
+
+            <router-link :to="'/projects/' + project.id" class="inline-flex items-center text-sm font-medium">
+                <img class=" w-full object-cover" :src="resolveIllustration(project.illustrations[0].path)" alt="Image du projet" />
             </router-link>
-            <div class="p-5 wrap overflow-hidden text-ellipsis">
+            <div class="p-5 wrap overflow-hidden text-lg text-ellipsis">
                 <h5>
                     {{ project.titre }}
                 </h5>
