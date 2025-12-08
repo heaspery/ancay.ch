1efb5e4b7291128876d87c1f6e31704b9dd52eb6
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-12-08 19:54:58 +0100

    fix route

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from '../components/TheHome.vue'
 import TheProjectDetails from '../components/TheProjectDetails.vue'
 import TheProjectList from '../components/TheProjectList.vue'
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
